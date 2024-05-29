45037031f33e692e0434427f214d79b839485443
Rotichgideon <gideonrotich2001@gmail.com>
2024-05-29 23:49:27 +0300

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
 # your code goes here!
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, words):
+        matched_anagrams = []
+        normalized_word = sorted(self.word)
+
+        for candidate in words:
+            if isinstance(candidate, str) and self.word != candidate.lower():
+                if sorted(candidate.lower()) == normalized_word:
+                    matched_anagrams.append(candidate)
+
+        return matched_anagrams
+    
